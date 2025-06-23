17b9bb633a417d974c8c3a6536e2b190248a151f
Michael Tiemann <github@michaeltiemann.net>
2025-06-23 22:03:50 +0200

    add main.jl for Whisper example

---
+++ examples/Whisper/main.jl
@@ -0,0 +1,9 @@
+using Lux,
+  Reactant
+
+function sinusoids(len, channels, max_timescale=10000)
+    log_timescale_increment = log(max_timescale) / (channels / 2 - 1)
+    inv_timescales = exp(-log_timescale_increment) * range(1, channels / 2)
+    scaled_time = range(1, len) * inv_timescales'
+    return cat(sin.(scaled_time), cos.(scaled_time); dims=2)
+end
